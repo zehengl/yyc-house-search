ebdcc96e02a2dd9e10d2b6086ce7cf8426a56778
Zeheng Li <imzehengl@gmail.com>
2022-05-30 00:28:07 -0600

    remove footer

--- web/src/layouts/AppLayout/AppLayout.js
+++ web/src/layouts/AppLayout/AppLayout.js
@@ -5,16 +5,6 @@ const AppLayout = ({ children }) => {
         <main>
           <div className="w-screen h-screen">{children}</div>
         </main>
-        <footer className="w-full">
-          <div className="flex items-center justify-center">
-            <a href="https://www.netlify.com">
-              <img
-                src="https://www.netlify.com/img/global/badges/netlify-light.svg"
-                alt="Deploys by Netlify"
-              />
-            </a>
-          </div>
-        </footer>
       </div>
     </>
   )
