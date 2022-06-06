a1da5b5aeba2c57e9ecd9e32f97f49f229e2c1e5
Zeheng Li <imzehengl@gmail.com>
2022-06-06 11:48:25 -0600

    change stops icon

--- web/src/components/LocationCell/LocationCell.js
+++ web/src/components/LocationCell/LocationCell.js
@@ -340,7 +340,12 @@ export const Success = ({
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
                 </div>
